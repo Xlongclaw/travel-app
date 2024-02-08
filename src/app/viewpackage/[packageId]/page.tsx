41bffbac787ffe23eb8686acbc28a6ea871ed0c3
unknown <xlongclaw@gmail.com>
2024-02-08 13:20:12 +0530

    completed packageDisplay design

--- src/app/viewpackage/[packageId]/page.tsx
+++ src/app/viewpackage/[packageId]/page.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import { getPackageDataByPackageId } from "../../../../sanity/sanity.query";
 import { packageType } from "@/app/tourpackages/types/type";
 import PackageDisplayWrapper from "./components/PackageDisplayWrapper";
+import Logo from "@/app/components/Logo";
 
 const page = async ({ params }: { params: { packageId: string } }) => {
   const packageData:packageType = await getPackageDataByPackageId(params.packageId);
   if (!packageData) return <div>LOADING</div>;
   return (
-    <div className="px-32 pt-8">
-      <NavBar />
+    <div className="px-32 pt-0 flex flex-col gap-6 justify-center items-center h-screen">
+      {/* <NavBar /> */}
       <PackageDisplayWrapper packageData={packageData}/>
     </div>
   );
